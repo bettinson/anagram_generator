3c985b5f61f87be5f9e4971e0c899fe7837b2c9b
Matt Bettinson <mattbettinson@me.com>
2012-10-28 21:54:30 -0400

    First commit. No functionality other than printing the letters to the screen

---
+++ README.md
@@ -0,0 +1,3 @@
+**Work in progress**
+
+A simple anagram generator. I developed this because I wanted to cream my friends in Letterpress.
---
+++ generator.py
@@ -0,0 +1,16 @@
+class generator:
+	def get_words():		#Grabs the words
+		global words
+		global letters
+		words = raw_input("input the letters\n")
+		letters = []	
+
+	def sort_words():		#Puts the words into an array
+		i = 0
+		for word in words:
+			letters.append(word)
+			print letters[i]
+			i = i + 1	
+	
+	get_words()
+	sort_words()
